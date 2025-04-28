bea20418ab28538012e05f02206e3b2058f6772f
JummerCloth <jchu0822@stanford.edu>
2025-04-28 14:54:24 -0700

    Removed all default rewards

--- pupperv3_mjx/config.py
+++ pupperv3_mjx/config.py
@@ -2,7 +2,9 @@ from ml_collections import config_dict
 
 
 def get_config():
-    """Returns reward config for barkour quadruped environment."""
+    """Returns reward config for barkour quadruped environment.
+       I have set all rewards to 0.0 to disable you from looking at the answers!
+    """
 
     def get_default_rewards_config():
         default_config = config_dict.ConfigDict(
@@ -16,52 +18,52 @@ def get_config():
                         # Tracking rewards are computed using exp(-delta^2/sigma)
                         # sigma can be a hyperparameters to tune.
                         # Track the base x-y velocity (no z-velocity tracking.)
-                        tracking_lin_vel=1.5,
+                        tracking_lin_vel=0.0,
                         # Track the angular velocity along z-axis, i.e. yaw rate.
-                        tracking_ang_vel=0.8,
+                        tracking_ang_vel=0.0,
                         # Below are regularization terms, we roughly divide the
                         # terms to base state regularizations, joint
                         # regularizations, and other behavior regularizations.
                         # Penalize the base velocity in z direction, L2 penalty.
-                        lin_vel_z=-2.0,
+                        lin_vel_z=0.0,
                         # Penalize the base roll and pitch rate. L2 penalty.
-                        ang_vel_xy=-0.05,
+                        ang_vel_xy=0.0,
                         # Penalize non-zero roll and pitch angles. L2 penalty.
-                        orientation=-5.0,
+                        orientation=0.0,
                         # Track desired body orientation
-                        tracking_orientation=1.0,
+                        tracking_orientation=0.0,
                         # L2 regularization of joint torques, |tau|^2.
-                        torques=-0.0002,
+                        torques=0.0,
                         # L2 reg of joint acceleration
-                        joint_acceleration=-1e-6,
+                        joint_acceleration=0.0,
                         # L1 regularization of mechanical work, |v * tau|.
-                        mechanical_work=-0.00,
+                        mechanical_work=0.0,
                         # Penalize the change in the action and encourage smooth
                         # actions. L2 regularization |action - last_action|^2
-                        action_rate=-0.01,
+                        action_rate=0.0,
                         # Encourage long swing steps.  However, it does not
                         # encourage high clearances.
-                        feet_air_time=0.2,
+                        feet_air_time=0.0,
                         # Encourage no motion at zero command, L1 regularization
                         # |q - q_default|.
-                        stand_still=-0.5,
+                        stand_still=0.0,
                         # Encourage no motion at zero command, L1 regularization
                         # |q_dot|.
-                        stand_still_joint_velocity=-0.1,
+                        stand_still_joint_velocity=0.0,
                         # Abduction angle penalty
-                        abduction_angle=-0.1,
+                        abduction_angle=0.0,
                         # Early termination penalty.
-                        termination=-100.0,
+                        termination=0.0,
                         # Penalizing foot slipping on the ground.
-                        foot_slip=-0.1,
+                        foot_slip=0.0,
                         # Penalize knee hitting ground
-                        knee_collision=-1.0,
+                        knee_collision=0.0,
                         # Penalize body hitting ground,
-                        body_collision=-1.0,
+                        body_collision=0.0,
                     )
                 ),
                 # Tracking reward = exp(-error^2/sigma).
-                tracking_sigma=0.25,
+                tracking_sigma=0.0,
             )
         )
         return default_config
